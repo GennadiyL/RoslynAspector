2e60cbd217f1078b75a5fa564d9ae9fb4547792d
GennadiyL <gena.vl@gmail.com>
2025-11-16 20:39:06 +0100

    Bug fixing in Demo App

--- RoslynAspectorDemo/TotalLoggingDemoApp/Program.cs
+++ RoslynAspectorDemo/TotalLoggingDemoApp/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace TotalLoggingDemoApp
+using TotalLoggingDemoLib;
+
+namespace TotalLoggingDemoApp;
+
+internal class Program
 {
-	internal class Program
+	private static async Task Main()
 	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello, World!");
-		}
+		Rect rect = new Rect();
+		rect.Left = 10;
+		rect.Top = 20;
+		rect.Width = 100;
+		rect.Height = 200;
+
+		rect.Move(10, 20);
+		Console.WriteLine($"Right: {rect.GetRight()}");
+		Console.WriteLine($"Bottom: {rect.GetBottom()}");
+
+		await rect.MoveAsync(15, 25);
+		Console.WriteLine($"Right: {await rect.GetRightAsync()}");
+		Console.WriteLine($"Bottom: {await rect.GetBottomAsync()}");
 	}
 }
