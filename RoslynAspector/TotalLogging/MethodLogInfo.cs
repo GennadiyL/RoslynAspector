a46b803a6581a2b01d57b384fd4e9f7f3a095d92
GennadiyL <gena.vl@gmail.com>
2025-11-16 22:15:13 +0100

    Renaming

--- RoslynAspector/TotalLogging/MethodLogInfo.cs
+++ RoslynAspector/TotalLogging/MethodLogInfo.cs
@@ -2,9 +2,9 @@
 
 namespace RoslynAspector.TotalLogging
 {
-	internal record LogMethodInfo
+	internal record MethodLogInfo
 	{
-		public LogWrapperLevel OnEntryLogLevel { get; init; }
+		public LogWrapperLevel OnEnterLogLevel { get; init; }
 		public LogWrapperLevel OnExitLogLevel { get; init; }
 		public LogWrapperLevel OnErrorLogLevel { get; init; }
 		public bool IsExceptionLoggingEnabled { get; init; }
